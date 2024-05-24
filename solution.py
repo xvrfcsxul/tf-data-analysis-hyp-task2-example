213cde7ba8933ac168fcb6ec1889b185f4179882
Andrey Yurlov <76644125+xvrfcsxul@users.noreply.github.com>
2024-05-24 16:41:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 399364172
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    correlation, p_value = stats.pearsonr(x, y)
+    return p_value < 0.05
